962778a5eafdd86035760bdf86087ea4d8843e7c
Henry Wang <henry.reiwang@gmail.com>
2025-12-16 23:50:26 -0800

    style: project name change

---
+++ src/siphon/cli.py
@@ -0,0 +1,28 @@
+# src/github_folder_downloader/cli.py
+import typer
+from typing_extensions import Annotated
+
+from .core import download_folder 
+
+app = typer.Typer(help="A fast, non-cloning CLI tool to download specific GitHub sub-directories.")
+
+@app.command()
+def main_cli(
+    repo: Annotated[str, typer.Argument(help="Include your target repo's name")],
+    path: Annotated[str, typer.Argument(help="The sub-directory path within the repository (e.g., 'scripts').")],
+    output: Annotated[str, typer.Option("--output", "-o", help="Directory to donwload in", show_default='cwd')] = None,
+    token: Annotated[str,typer.Option("--token", "-t", help="GitHub Personal Access Token for higher rate limits (optional).")] = None):
+    
+    try:
+        download_folder(repo, path, output, token)
+        typer.echo("\n Operation complete!")
+    except typer.Exit as e:
+        raise e
+    except Exception as e:
+        typer.echo(f"\n A critical error occurred: {e}", err=True)
+        raise typer.Exit(code=1)
+ 
+if __name__ == "__main__":
+    app()
+
+
---
+++ src/siphon/threading_manager.py
@@ -0,0 +1,32 @@
+# src/getthefiles/threading_manager.py
+
+import concurrent.futures
+from typing import List, Tuple, Optional
+import typer
+import requests
+
+DownloadJob = Tuple[str, str] 
+
+class ThreadingManager:
+    MAX_WORKERS = 8 
+    def __init__(self, download_jobs: List[DownloadJob], session: requests.Session):
+        self.jobs = download_jobs
+        self.session = session
+        
+    def execute_downloads(self) -> None:
+        """Submits all download jobs to a ThreadPoolExecutor."""
+        from .core import fetch_and_save 
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.MAX_WORKERS) as executor:
+            future_to_job = {executor.submit(fetch_and_save, url, path, self.session): (url, path) 
+                for url, path in self.jobs}
+            
+            for future in concurrent.futures.as_completed(future_to_job):
+                url, path = future_to_job[future]
+                try:
+                    success = future.result() 
+                    if success:
+                        typer.echo(f"  [DONE] {path}", color=typer.colors.GREEN)
+                    else:
+                        typer.echo(f"  [FAIL] Failed to download {path}", color=typer.colors.RED, err=True)
+                except Exception as exc:
+                    typer.echo(f"  [ERROR] {path} generated an exception: {exc}", color=typer.colors.RED, err=True)
